6178c8bcfb7d37f4bd86b05bc67d9d71f7d9bc98
Gonzalo Robaina <gonzalor@gmail.com>
2022-05-23 15:39:06 -0300

    Improve card styles

--- index.js
+++ index.js
@@ -4,21 +4,69 @@ import terminalLink from 'terminal-link';
 import data from './lib/data.js';
 
 const name = `${chalk.bold(data.name)}
-${chalk.red(data.title_highlight)} ${chalk.white(data.title)}`;
+${chalk.red(data.title_highlight)} ${chalk.white(data.title)}
+${chalk.gray(data.subtitle)}
+------------------------------
+${Object.keys(data.urls).forEach((key) => {
+  return terminalLink(chalk.yellow(key), data.urls[key]);
+})}
+`;
 
 const render = (args) => {
   const { json } = args;
   const output = [];
 
   if (json) {
-    output.push(JSON.stringify(data, null, 2));
-  } else {
-    output.push(boxen(name, { padding: 1, margin: 2 }));
-
-    Object.keys(data.urls).forEach((key) => {
-      output.push(terminalLink(chalk.yellow(key), data.urls[key]));
-    });
+    return JSON.stringify(data, null, 2);
   }
+
+  const topBox = [];
+  const bottomBox = [];
+
+  topBox.push(chalk.bold(data.name));
+  topBox.push(`${chalk.red(data.title_highlight)} ${chalk.white(data.title)}`);
+  topBox.push(`${chalk.gray(data.subtitle)}`);
+
+  output.push(boxen(topBox.join('\n'), {
+    padding: {
+      top: 1,
+      bottom: 1,
+      left: 3,
+      right: 20,
+    },
+    margin: {
+      top: 0,
+      bottom: 0,
+      left: 6,
+      right: 0,
+    },
+    borderColor: 'red',
+  }));
+
+  Object.keys(data.urls).forEach((key) => {
+    bottomBox.push(terminalLink(chalk.yellow(key), data.urls[key]));
+  });
+
+  output.push(boxen(bottomBox.join('\n'), {
+    padding: {
+      top: 0,
+      left: 3,
+    },
+    margin: {
+      top: 0,
+      left: 6
+    },
+    borderStyle: {
+      topLeft: ' ',
+      topRight: ' ',
+      bottomLeft: ' ',
+      bottomRight: ' ',
+      top: ' ',
+      bottom: ' ',
+      left: ' ',
+      right: ' '
+    },
+  }));
 
   return output.join('\n');
 };
